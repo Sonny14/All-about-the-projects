bb453c2cbb26b56a85750272cf68899e5c58765f
hp <hp@DESKTOP-VDURALK>
2024-11-14 18:18:00 +0800

    login and signup using txt file

---
+++ src/project/Login.java
@@ -0,0 +1,195 @@
+package project;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Login extends JFrame implements ActionListener {
+
+    JLabel label00, label01, label02;
+    JTextField textField01; // ID field
+    JPasswordField passField; // Password field
+    JButton loginBtn, clearBtn; // Login and clear buttons
+    JPanel panel01; // Panel
+    File file = new File("C:\\Files"); // File path
+    int lines;
+
+    // Constructor
+    Login() {
+
+        // Panels
+        panel01 = new JPanel();
+        panel01.setSize(510, 780);
+        panel01.setLayout(null);
+        panel01.setBackground(Color.blue);
+        //add(panel01);
+
+        // Labels
+        label00 = new JLabel("Login to Cinetix");
+        label00.setFont(new Font("Sans Serif", Font.BOLD, 24));
+        label01 = new JLabel("Enter Email:");
+        label01.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        label02 = new JLabel("Enter Password:");
+        label02.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        add(label00);
+        add(label01);
+        add(label02);
+        label00.setBounds(160, 50, 200, 30);
+        label01.setBounds(50, 150, 100, 20);
+        label02.setBounds(50, 230, 100, 20);
+
+        // Text Fields
+        textField01 = new JTextField();
+        passField = new JPasswordField();
+        add(textField01);
+        add(passField);
+        textField01.setBounds(160, 150, 250, 35);
+        passField.setBounds(160, 230, 250, 35);
+
+        // Buttons
+        loginBtn = new JButton("Login");
+        clearBtn = new JButton("Clear");
+        loginBtn.addActionListener(this);
+        clearBtn.addActionListener(this);
+        add(loginBtn);
+        add(clearBtn);
+        loginBtn.setBounds(275, 310, 130, 35);
+        clearBtn.setBounds(130, 310, 130, 35);
+        add(panel01);
+        
+        //Frame Properties
+        this.setTitle("Login");
+        this.setSize(520, 520);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    
+    }
+
+    // Create a folder
+    void createFolder() {
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
+
+    // Read text file
+    void readFile() {
+        try {
+            FileReader fileReader = new FileReader(file + "\\logins.txt");
+            System.out.println("File exists!");
+        } catch (FileNotFoundException ex) {
+            try {
+                FileWriter fileWriter = new FileWriter(file + "\\logins.txt");
+                System.out.println("File was created");
+            } catch (IOException ex1) {
+                ex1.printStackTrace();
+            }
+        }
+    }
+
+    // Login logic
+    void logic(String user, String pass) throws IOException{
+        try{
+            RandomAccessFile raf = new RandomAccessFile(file+"\\logins.txt", "rw");
+            for(int i = 0; i < lines; i+=5){
+                System.out.println("Count: "+i);
+                String lineUser = raf.readLine();
+                String linePass = raf.readLine();
+                
+                if(lineUser != null && linePass != null){
+                    String forUser = lineUser.substring(9);
+                    String forPass = linePass.substring(9);
+                   
+//                   String forUser = lineUser;
+//                   String forPass = linePass;
+                    System.out.println("User: "+forUser+"Pass: "+forPass);
+                    
+                    if(user.equals(forUser) && pass.equals(forPass)){
+                        JOptionPane.showMessageDialog(null, "Login Successfully!!"); 
+                        break;
+                    }else if(i == (lines - 5)){
+                        //Adjusting index to lines count
+                        JOptionPane.showMessageDialog(null, "Incorrect username/password"); 
+                        break;
+                    }else{
+                        System.out.println("Skipped invalid line with insufficient length or null:"+lineUser+", "+linePass);
+                    }
+                    //Skip next lines
+                    for (int k = 1; k <= 5; k++) { if (raf.readLine() == null) { break;
+                }
+            }
+        }
+    }
+        }catch (FileNotFoundException ex) { ex.printStackTrace();
+    }
+    }
+
+
+    // Count the number of lines in the file
+    void countLines() {
+        try {
+            lines = 0;
+            RandomAccessFile raf = new RandomAccessFile(file + "\\logins.txt", "rw");
+            while (raf.readLine() != null) {
+                lines++;
+            }
+            System.out.println("Number of lines: " + lines);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Action performed
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginBtn) {
+            String str1 = textField01.getText();
+            char[] p = passField.getPassword();
+            String str2 = new String(p);
+
+            try {
+                createFolder();
+                readFile();
+                countLines();
+                logic(str1, str2);
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        } else {
+            textField01.setText("");
+            passField.setText("");
+        }
+    }
+    
+    void listDirectoryContents() {
+    File dir = new File("C:\\Files");
+    if (dir.exists() && dir.isDirectory()) {
+        String[] files = dir.list();
+        if (files != null) {
+            for (String fileName : files) {
+                System.out.println("Found file: " + fileName);
+            }
+        } else {
+            System.out.println("Directory is empty.");
+        }
+    } else {
+        System.out.println("Directory does not exist.");
+    }
+}
+
+
+    public static void main(String arr[]) {
+        Login login = new Login();
+        login.listDirectoryContents();
+    }
+}
---
+++ src/project/SignUp.java
@@ -0,0 +1,245 @@
+
+/*
+ * Click nbfs
+://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import javax.swing.*;
+
+/**
+ *
+ * @author hp
+ */
+public class SignUp extends JFrame implements ActionListener{
+    
+    //Declaration of variables
+    JLabel label00, label01, label02, label03, label04, label05, label06;
+    JTextField textField01, textField02, textField03, textField04; 
+    JButton btn1, btn2;
+    JPanel panel01;
+    JPasswordField passwordField, confirmField;
+    File file = new File("C:\\Files");
+    int lines;
+    
+    //Constructor
+    SignUp(){
+        
+        //Frame properties
+        this.setVisible(true);
+        this.setSize(1020, 780);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setResizable(false);
+        this.setTitle("Sign-up Cinetix");
+        this.setLayout(null);
+        
+        //Panels
+        panel01 = new JPanel();
+        panel01.setSize(510, 780);
+        panel01.setLayout(null);
+        panel01.setBackground(Color.blue);
+        add(panel01);
+        
+        //Labels
+        label00 = new JLabel("Add Employee");
+        label00.setFont(new Font("Sans Serif", Font.BOLD, 24));
+        label01 = new JLabel("First Name");
+        label01.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        label02 = new JLabel("Last Name");
+        label02.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        label03 = new JLabel("Employee ID");
+        label03.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        label04 = new JLabel("Email");
+        label04.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+//        label05 = new JLabel("Employee ID");
+//        label05.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        label05 = new JLabel("Password");
+        label05.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        label06 = new JLabel("Confirm password");
+        label06.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+        add(label00);
+        add(label01);
+        add(label02);
+        add(label03);
+        add(label04);
+        //add(label05);
+        add(label05);
+        add(label06);
+        label00.setBounds(680, 90, 200, 30);
+        label01.setBounds(580, 150, 100, 20);
+        label02.setBounds(790, 150, 100, 20);
+        label03.setBounds(580, 230, 100, 20);
+        label04.setBounds(580, 310, 100, 20);
+        label05.setBounds(580, 390, 150, 20);
+        label06.setBounds(580, 470, 150, 20);
+        
+        
+        //Text Fields
+        textField01 = new JTextField();
+        textField02 = new JTextField();
+        textField03 = new JTextField();
+        textField04 = new JTextField();
+        //textField05 = new JTextField();
+        passwordField = new JPasswordField();
+        confirmField = new JPasswordField();
+        
+        add(textField01);
+        add(textField02);
+        add(textField03);
+        add(textField04);
+        //add(textField05);
+        add(passwordField);
+        add(confirmField);
+        textField01.setBounds(580, 170, 150, 35);
+        textField02.setBounds(790, 170, 150, 35);
+        textField03.setBounds(580, 250, 250, 35);
+        textField04.setBounds(580, 330, 250, 35);
+        //textField05.setBounds(580, 410, 250, 35);
+        passwordField.setBounds(580, 410, 250, 35);
+        confirmField.setBounds(580, 490, 250, 35);
+        
+        
+        
+        //Buttons
+        btn1 = new JButton("Submit");
+        btn2 = new JButton("Clear");
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        add(btn1);
+        add(btn2);
+        btn1.setBounds(785, 570, 130, 35);
+        btn2.setBounds(610, 570, 130, 35);
+    }
+    
+    //Create a folder
+    void createFolder(){
+        if(!file.exists()){
+            file.mkdirs();
+        }
+    }
+    
+    //Read text file
+    void readFile() throws IOException{
+        try{
+            FileReader fileReader  = new FileReader(file+"\\logins.txt");
+            System.out.println("File do exist");
+        }catch(FileNotFoundException ex){
+            try{
+                FileWriter fileWriter = new FileWriter(file+"\\logins.txt");
+                System.out.println("File was created");
+            }catch(IOException ex1){ 
+            }
+        }
+    }
+    
+    void addData(String firstName, String lastName, String user, String pass, String email){
+        try{
+            RandomAccessFile randAccFile = new RandomAccessFile(file+"\\logins.txt", "rw");
+            
+            //Loop until the last line
+            for(int i = 0; i < lines; i++){
+                randAccFile.readLine();
+            }
+            
+            if(lines > 0){
+                randAccFile.writeBytes("\r\n");
+                randAccFile.writeBytes("\r\n");
+            }
+            randAccFile.writeBytes("Username:"+user+"\r\n");
+            randAccFile.writeBytes("Password:"+pass+"\r\n");
+            randAccFile.writeBytes("First Name:"+firstName+"\r\n");
+            randAccFile.writeBytes("Last Name:"+lastName+"\r\n");
+            randAccFile.writeBytes("Email:"+email+"\r\n");
+            
+        }
+        catch(FileNotFoundException ex){}
+        catch(IOException ex){}
+    }
+    
+    void countLines(){
+        try{
+            lines = 0;
+            RandomAccessFile randomAccFile = new RandomAccessFile(file+"\\logins.txt", "rw");
+            for(int i = 0; randomAccFile.readLine() != null; i++){
+                lines++;
+            }
+            System.out.println("Number of lines: "+lines);
+        } 
+        catch(FileNotFoundException ex){}
+        catch(IOException ex){}
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == btn1){
+            int x = 0;
+            String s1 = textField01.getText();
+            String s2 = textField02.getText();
+            char[] s3 = passwordField.getPassword();
+            char[] s4 = confirmField.getPassword();
+            String s5 = textField03.getText();
+            String s6 = textField04.getText();
+            String s7 = new String(s3);
+            String s8 = new String(s4);
+            
+            if(s7.isEmpty() || s8.isEmpty()){
+                System.out.println("Password is null");
+                JOptionPane.showMessageDialog(this, "Password is empty!", "Warning!", JOptionPane.ERROR_MESSAGE);
+            }
+            else if(s1.isEmpty() && s2.isEmpty()){
+                System.out.println("User name is null");
+                JOptionPane.showMessageDialog(this, "Firstname and Lastname is empty", "Warning!", JOptionPane.ERROR_MESSAGE);
+            }
+            else if(s5.isEmpty()){
+                System.out.println("Employee id is null");
+                JOptionPane.showMessageDialog(this, "Employee Id is empty", "Warning!", JOptionPane.ERROR_MESSAGE);
+            }
+            else if(s6.isEmpty()){
+                System.out.println("Email is null");
+                JOptionPane.showMessageDialog(this, "Email is empty", "Warning!", JOptionPane.ERROR_MESSAGE);
+            }
+            else if(s7.equals(s8)){
+                try{
+                    createFolder();
+                    readFile();
+                    countLines();
+                    addData(s1, s2, s5, s7, s6);
+                    
+                    JOptionPane.showMessageDialog(null, "Employee is added successfully");
+                    new Login();
+                    this.dispose();
+                }catch(Exception ex){
+                    System.out.println(ex);
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Password do not match");
+            }
+        }else{
+            textField01.setText("");
+            textField02.setText("");
+            textField03.setText("");
+            textField04.setText("");
+            passwordField.setText("");
+            confirmField.setText("");
+        }
+    }
+    
+    
+        
+    public static void main(String[] args) {
+        SignUp signUp = new SignUp();
+        signUp.setVisible(true);
+    }
+    
+}
