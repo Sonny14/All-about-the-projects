ff04cb9fc1f49e2e3d4d8ff7253c773f6257f202
hp <hp@DESKTOP-VDURALK>
2024-11-14 18:21:26 +0800

    login and signup using txt file

--- src/LoginPortal/Login.java
+++ src/LoginPortal/Login.java
@@ -1,4 +1,4 @@
-package project;
+package LoginPortal;
 
 import javax.swing.*;
 import java.awt.*;
--- src/LoginPortal/SignUp.java
+++ src/LoginPortal/SignUp.java
@@ -4,7 +4,7 @@
 ://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package project;
+package LoginPortal;
 
 import java.awt.Color;
 import java.awt.Font;
